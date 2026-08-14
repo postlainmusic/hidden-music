209ca7db3735936cc9fb609f4b3e82b656988ebe
postlainmusic <admin@postlainmusic.com>
2026-08-14 08:50:33 +0700

    feat: setup .gitignore and update to new supabase configuration

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -16,9 +16,9 @@ export async function GET(request: Request) {
     const cookieStore = cookies();
     const response = NextResponse.redirect(`${origin}${next}`);
 
-    const rawUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://yodctlkebsbtivmkskdo.supabase.co';
-    const supabaseUrl = rawUrl.replace('yodctlkebsbtivmkskdosb', 'yodctlkebsbtivmkskdo');
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvZGN0bGtlYnNidGl2bWtza2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NTY3MDcsImV4cCI6MjEwMTMzMjcwN30.3m5xKIcqU4ZaDeNSrnrk_XzqV7r_BGnshTFrsFJDBqw';
+    const rawUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://muemwfqynfljpmvxmpep.supabase.co';
+    const supabaseUrl = rawUrl.replace('muemwfqynfljpmvxmpepsb', 'muemwfqynfljpmvxmpep');
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11ZW13ZnF5bmZsanBtdnhtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTIwMDYsImV4cCI6MjEwMjIyODAwNn0.btnyUiVm-KqKlGQ-PlhVdPBy-VP005ltDMzABIHfYro';
 
     const supabase = createServerClient(
       supabaseUrl,
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,10 +4,10 @@ import { cookies } from 'next/headers';
 export function createClient() {
   const cookieStore = cookies();
 
-  const rawUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://yodctlkebsbtivmkskdo.supabase.co';
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvZGN0bGtlYnNidGl2bWtza2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NTY3MDcsImV4cCI6MjEwMTMzMjcwN30.3m5xKIcqU4ZaDeNSrnrk_XzqV7r_BGnshTFrsFJDBqw';
+  const rawUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://muemwfqynfljpmvxmpep.supabase.co';
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11ZW13ZnF5bmZsanBtdnhtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTIwMDYsImV4cCI6MjEwMjIyODAwNn0.btnyUiVm-KqKlGQ-PlhVdPBy-VP005ltDMzABIHfYro';
 
-  const supabaseUrl = rawUrl.replace('yodctlkebsbtivmkskdosb', 'yodctlkebsbtivmkskdo');
+  const supabaseUrl = rawUrl.replace('muemwfqynfljpmvxmpepsb', 'muemwfqynfljpmvxmpep');
 
   return createServerClient(
     supabaseUrl,
