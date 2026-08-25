ebfb1907d807bf5dec217b2034c1019908852311
postlainmusic <postlain.music@gmail.com>
2026-08-25 18:52:21 +0700

    Update MobilePlayerBar.tsx

--- src/components/ui/player/MobilePlayerBar.tsx
+++ src/components/ui/player/MobilePlayerBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useMemo, useEffect, useRef, useCallback } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'motion/react';
 import {
   Play,
   Pause,
